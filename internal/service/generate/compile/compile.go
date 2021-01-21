de183e70f8efa551ffd11e74e15eeb324d854aaa
Milad Irannejad <milad@uber.com>
2021-01-20 22:38:33 -0500

    gen: generating empty structs for builders

--- internal/service/generate/compile/compile.go
+++ internal/service/generate/compile/compile.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"go/ast"
 	"go/token"
+	"strings"
 
 	"golang.org/x/tools/go/ast/astutil"
 
@@ -12,7 +13,14 @@ import (
 
 // Compiler creates test helpers (mocks, factories, builders, etc.) for a Go file.
 type Compiler struct {
-	logger  *log.ColorfulLogger
+	logger *log.ColorfulLogger
+	state  struct {
+		TypeName string
+	}
+	inputs struct {
+		pkgPath string
+		pkgName string
+	}
 	outputs struct {
 		file *ast.File
 	}
@@ -27,7 +35,15 @@ func New(logger *log.ColorfulLogger) *Compiler {
 
 // Compile takes an ast.File node and generates a new ast.File node with test helpers (mocks, factories, builders, etc.).
 func (c *Compiler) Compile(pkgPath, pkgName string, file *ast.File) *ast.File {
-	c.outputs.file = &ast.File{
+	c.inputs.pkgPath = pkgPath
+	c.inputs.pkgName = pkgName
+
+	astutil.Apply(file, c.pre, c.post)
+	return c.outputs.file
+}
+
+func createFile(pkgPath, pkgName string) *ast.File {
+	return &ast.File{
 		Name: &ast.Ident{
 			Name: pkgName + "test",
 		},
@@ -45,18 +61,70 @@ func (c *Compiler) Compile(pkgPath, pkgName string, file *ast.File) *ast.File {
 			},
 		},
 	}
+}
 
-	astutil.Apply(file, c.pre, c.post)
-
-	return c.outputs.file
+func createBuilderStruct(pkgName, typeName string) *ast.GenDecl {
+	return &ast.GenDecl{
+		Tok: token.TYPE,
+		Specs: []ast.Spec{
+			&ast.TypeSpec{
+				Name: &ast.Ident{
+					Name: typeName + "Builder",
+				},
+				Type: &ast.StructType{
+					Fields: &ast.FieldList{
+						List: []*ast.Field{
+							{
+								Type: &ast.SelectorExpr{
+									X:   &ast.Ident{Name: pkgName},
+									Sel: &ast.Ident{Name: typeName},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
 }
 
 // Pre is called for each node before the node's children are traversed (pre-order).
-func (c *Compiler) pre(cursor *astutil.Cursor) bool {
-	return true
+func (c *Compiler) pre(cr *astutil.Cursor) bool {
+	switch n := cr.Node().(type) {
+	case *ast.File:
+		c.outputs.file = createFile(c.inputs.pkgPath, c.inputs.pkgName)
+		return true
+
+	case *ast.GenDecl:
+		switch n.Tok {
+		case token.IMPORT:
+		case token.TYPE:
+			return true
+		}
+
+	case *ast.TypeSpec:
+		// Continue only if the type is exported
+		if name := n.Name.Name; name == strings.Title(name) {
+			c.state.TypeName = name
+			return true
+		}
+
+	case *ast.StructType:
+		builderStruct := createBuilderStruct(c.inputs.pkgName, c.state.TypeName)
+		c.outputs.file.Decls = append(c.outputs.file.Decls, builderStruct)
+
+	case *ast.InterfaceType:
+	}
+
+	return false
 }
 
 // Post is called for each node after its children are traversed (post-order).
-func (c *Compiler) post(cursor *astutil.Cursor) bool {
+func (c *Compiler) post(cr *astutil.Cursor) bool {
+	switch cr.Node().(type) {
+	case *ast.TypeSpec:
+		c.state.TypeName = ""
+	}
+
 	return true
 }
--- internal/service/generate/compile/compile_test.go
+++ internal/service/generate/compile/compile_test.go
@@ -22,6 +22,169 @@ func TestCompiler(t *testing.T) {
 		White:   logger,
 	}
 
+	inputFile := &ast.File{
+		Name: &ast.Ident{
+			Name: "lookup",
+		},
+		Decls: []ast.Decl{
+			// Imports
+			&ast.GenDecl{
+				Tok: token.IMPORT,
+				Specs: []ast.Spec{
+					&ast.ImportSpec{
+						Path: &ast.BasicLit{
+							Value: "fmt",
+						},
+					},
+				},
+			},
+			// Structs
+			&ast.GenDecl{
+				Tok: token.TYPE,
+				Specs: []ast.Spec{
+					&ast.TypeSpec{
+						Name: &ast.Ident{Name: "Request"},
+						Type: &ast.StructType{
+							Fields: &ast.FieldList{
+								List: []*ast.Field{
+									{
+										Names: []*ast.Ident{
+											&ast.Ident{Name: "ID"},
+										},
+										Type: &ast.Ident{Name: "string"},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			&ast.GenDecl{
+				Tok: token.TYPE,
+				Specs: []ast.Spec{
+					&ast.TypeSpec{
+						Name: &ast.Ident{Name: "Response"},
+						Type: &ast.StructType{
+							Fields: &ast.FieldList{
+								List: []*ast.Field{
+									{
+										Names: []*ast.Ident{
+											&ast.Ident{Name: "Name"},
+										},
+										Type: &ast.Ident{Name: "string"},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			// Interfaces
+			&ast.GenDecl{
+				Tok: token.TYPE,
+				Specs: []ast.Spec{
+					&ast.TypeSpec{
+						Name: &ast.Ident{Name: "Service"},
+						Type: &ast.InterfaceType{
+							Methods: &ast.FieldList{
+								List: []*ast.Field{
+									{
+										Names: []*ast.Ident{
+											&ast.Ident{Name: "Lookup"},
+										},
+										Type: &ast.FuncType{
+											Params: &ast.FieldList{
+												List: []*ast.Field{
+													{
+														Type: &ast.StarExpr{
+															X: &ast.Ident{Name: "Request"},
+														},
+													},
+												},
+											},
+											Results: &ast.FieldList{
+												List: []*ast.Field{
+													{
+														Type: &ast.StarExpr{
+															X: &ast.Ident{Name: "Response"},
+														},
+													},
+													{
+														Type: &ast.Ident{Name: "error"},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	expectedFile := &ast.File{
+		Name: &ast.Ident{
+			Name: "exampletest",
+		},
+		Decls: []ast.Decl{
+			// Imports
+			&ast.GenDecl{
+				Tok: token.IMPORT,
+				Specs: []ast.Spec{
+					&ast.ImportSpec{
+						Path: &ast.BasicLit{
+							Value: `"github.com/octocat/app/example"`,
+						},
+					},
+				},
+			},
+			// Structs
+			&ast.GenDecl{
+				Tok: token.TYPE,
+				Specs: []ast.Spec{
+					&ast.TypeSpec{
+						Name: &ast.Ident{Name: "RequestBuilder"},
+						Type: &ast.StructType{
+							Fields: &ast.FieldList{
+								List: []*ast.Field{
+									{
+										Type: &ast.SelectorExpr{
+											X:   &ast.Ident{Name: "example"},
+											Sel: &ast.Ident{Name: "Request"},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			&ast.GenDecl{
+				Tok: token.TYPE,
+				Specs: []ast.Spec{
+					&ast.TypeSpec{
+						Name: &ast.Ident{Name: "ResponseBuilder"},
+						Type: &ast.StructType{
+							Fields: &ast.FieldList{
+								List: []*ast.Field{
+									{
+										Type: &ast.SelectorExpr{
+											X:   &ast.Ident{Name: "example"},
+											Sel: &ast.Ident{Name: "Response"},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
 	tests := []struct {
 		name         string
 		pkgPath      string
@@ -30,28 +193,11 @@ func TestCompiler(t *testing.T) {
 		expectedFile *ast.File
 	}{
 		{
-			name:    "OK",
-			pkgPath: "github.com/octocat/app/example",
-			pkgName: "example",
-			file:    &ast.File{},
-			expectedFile: &ast.File{
-				Name: &ast.Ident{
-					Name: "exampletest",
-				},
-				Decls: []ast.Decl{
-					// Imports
-					&ast.GenDecl{
-						Tok: token.IMPORT,
-						Specs: []ast.Spec{
-							&ast.ImportSpec{
-								Path: &ast.BasicLit{
-									Value: `"github.com/octocat/app/example"`,
-								},
-							},
-						},
-					},
-				},
-			},
+			name:         "OK",
+			pkgPath:      "github.com/octocat/app/example",
+			pkgName:      "example",
+			file:         inputFile,
+			expectedFile: expectedFile,
 		},
 	}
 
